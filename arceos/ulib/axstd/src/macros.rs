468d8678125a1cd6a3ceedf328379d9a18a204b8
winddevil <1160712160@qq.com>
2024-11-14 20:46:31 +0800

    仅仅修改axsrd的println!和print!

--- arceos/ulib/axstd/src/macros.rs
+++ arceos/ulib/axstd/src/macros.rs
@@ -9,7 +9,7 @@
 #[macro_export]
 macro_rules! print {
     ($($arg:tt)*) => {
-        $crate::io::__print_impl(format_args!($($arg)*));
+        $crate::io::__print_impl(format_args!("\x1b[31m{}\x1b[0m", format_args!($($arg)*)));
     }
 }
 
@@ -18,6 +18,6 @@ macro_rules! print {
 macro_rules! println {
     () => { $crate::print!("\n") };
     ($($arg:tt)*) => {
-        $crate::io::__print_impl(format_args!("{}\n", format_args!($($arg)*)));
+        $crate::io::__print_impl(format_args!("\x1b[31m{}\n\x1b[0m", format_args!($($arg)*)));
     }
 }
