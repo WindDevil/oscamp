d7a54e4ec60049e8f855e86bb00df5b8a506c404
winddevil <1160712160@qq.com>
2024-11-21 15:50:08 +0800

    暂存一下当前进度

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -3,30 +3,48 @@
 #![no_std]
 #![allow(unused_variables)]
 
-use allocator::{BaseAllocator, ByteAllocator, AllocResult};
+use allocator::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
 use core::ptr::NonNull;
 use core::alloc::Layout;
 
-pub struct LabByteAllocator;
+pub struct LabByteAllocator{
+    start: usize,
+    end: usize,
+    byte_pos: usize,
+}
 
 impl LabByteAllocator {
     pub const fn new() -> Self {
-        Self
+        Self{
+            start: 0,
+            end: 0,
+            byte_pos: 0,
+        }
     }
 }
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        unimplemented!();
+        self.start = start;
+        self.end = start + size;
+        self.byte_pos = start;
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
-        unimplemented!();
+        self.start = start;
+        self.end = start + size;
+        self.byte_pos = start;
+        AllocResult::Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
-        unimplemented!();
+        let len = layout.size();
+        let new_pos = self.byte_pos + len;
+        if new_pos > self.end {
+            return AllocResult::Err(AllocError::NoMemory);
+        }
+        AllocResult::Ok(NonNull::new( as *mut u8).unwrap())
     }
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
         unimplemented!();
