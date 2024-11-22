a2759ea7f45b073e257233d2f23e7c931581d488
winddevil <1160712160@qq.com>
2024-11-22 09:49:14 +0800

    引入axlog观察测例,并且固定pool的占用位置

--- arceos/labs/lab_allocator/src/lib.rs
+++ arceos/labs/lab_allocator/src/lib.rs
@@ -6,8 +6,14 @@
 use allocator::{AllocError, AllocResult, BaseAllocator, ByteAllocator};
 use core::ptr::NonNull;
 use core::alloc::Layout;
+use axlog::*;
+
+const POOL_SIZE:usize = 96+192+384;
 
 pub struct LabByteAllocator{
+    pool_96: usize,
+    pool_192: usize,
+    pool_384: usize,
     start: usize,
     end: usize,
     byte_pos: usize,
@@ -16,6 +22,9 @@ pub struct LabByteAllocator{
 impl LabByteAllocator {
     pub const fn new() -> Self {
         Self{
+            pool_96: 0,
+            pool_192: 0,
+            pool_384: 0,
             start: 0,
             end: 0,
             byte_pos: 0,
@@ -25,20 +34,33 @@ impl LabByteAllocator {
 
 impl BaseAllocator for LabByteAllocator {
     fn init(&mut self, start: usize, size: usize) {
-        self.start = start;
+        self.pool_96 = start;
+        self.pool_192 = start+96;
+        self.pool_384 = start+96+192;
+        self.start = start+POOL_SIZE;
         self.end = start + size;
-        self.byte_pos = start;
+        self.byte_pos = self.start;
     }
     fn add_memory(&mut self, start: usize, size: usize) -> AllocResult {
         self.start = start;
         self.end = start + size;
-        self.byte_pos = start;
+        self.byte_pos = self.start;
         AllocResult::Ok(())
     }
 }
 
 impl ByteAllocator for LabByteAllocator {
     fn alloc(&mut self, layout: Layout) -> AllocResult<NonNull<u8>> {
+        if layout.align() == 8 {
+            debug!("alloc layout: {:?}", layout);
+            if layout.size() == 96 {
+                return AllocResult::Ok(NonNull::new(self.pool_96 as *mut u8).unwrap());
+            } else if layout.size() == 192 {
+                return AllocResult::Ok(NonNull::new(self.pool_192 as *mut u8).unwrap());
+            } else if layout.size() == 384 {
+                return AllocResult::Ok(NonNull::new(self.pool_384 as *mut u8).unwrap());
+            }
+        }
         let len = layout.size();
         let new_pos = self.byte_pos + len;
         if new_pos > self.end {
@@ -49,6 +71,10 @@ impl ByteAllocator for LabByteAllocator {
         AllocResult::Ok(NonNull::new(ptr).unwrap())
     }
     fn dealloc(&mut self, pos: NonNull<u8>, layout: Layout) {
+        if layout.align() == 8 {
+            info!("dealloc layout: {:?}", layout);
+            return;
+        }
         let len = layout.size();
         let pos = pos.as_ptr() as usize;
         if pos+len == self.byte_pos {
