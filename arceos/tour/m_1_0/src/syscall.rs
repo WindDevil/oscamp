a195f8dffa7c8e840418a3d599a212c3205a6b69
winddevil <1160712160@qq.com>
2024-12-03 19:51:20 +0800

    完成tour_m_1_0

--- arceos/tour/m_1_0/src/syscall.rs
+++ arceos/tour/m_1_0/src/syscall.rs
@@ -1,7 +1,10 @@
 #![allow(dead_code)]
 
 use axhal::arch::TrapFrame;
-use axhal::trap::{register_trap_handler, SYSCALL};
+use axhal::mem::VirtAddr;
+use axhal::trap::{register_trap_handler, SYSCALL, PAGE_FAULT};
+use axhal::paging::MappingFlags;
+use axtask::TaskExtRef;
 use axerrno::LinuxError;
 
 const SYS_EXIT: usize = 93;
@@ -20,4 +23,24 @@ fn handle_syscall(tf: &TrapFrame, syscall_num: usize) -> isize {
         }
     };
     ret
+}
+
+#[register_trap_handler(PAGE_FAULT)]
+fn handle_page_fault(vaddr: VirtAddr, access_flags: MappingFlags, is_user: bool) -> bool {
+    if is_user {
+        if !axtask::current()
+            .task_ext()
+            .aspace
+            .lock()
+            .handle_page_fault(vaddr, access_flags)
+        {
+            ax_println!("{}: segmentation fault, exit!", axtask::current().id_name());
+            axtask::exit(-1);
+        } else {
+            ax_println!("{}: handle page fault OK!", axtask::current().id_name());
+        }
+        true
+    } else {
+        false
+    }
 }
